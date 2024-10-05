6e87913940e76084400a557772215a358c95611b
Dill <60818081+Di-Liks@users.noreply.github.com>
2024-10-05 12:57:22 +0300

    Update 3.go

--- 3.go
+++ 3.go
@@ -1,21 +1,30 @@
 package main
+
 import "fmt"
-type Mutex struct { Count  int
- signal chan struct{} done   chan struct{}
+
+type Mutex struct {
+	Count  int
+	signal chan struct{}
+	done   chan struct{}
 }
-func (m *Mutex) Unlock() { m.signal <- struct{}{}
+
+func (m *Mutex) Unlock() {
+	m.signal <- struct{}{}
 }
-func (m *Mutex) Wait() { for i := 0; i < m.Count; i++ {
-  <-m.done }
-}
-func main() { m := Mutex{
-  Count:  3,  signal: make(chan struct{}, 3),
-  done:   make(chan struct{}, 3), }
- for i := 0; i < m.Count; i++ {
-  m.signal <- struct{}{} }
- for i := 0; i < 3; i++ {
-  go func() {   defer m.Unlock()
-   fmt.Println("Hello, 世界")   m.done <- struct{}{}
-  }() }
- m.Wait()
+
+func main() {
+	m := Mutex{
+		Count: 3, signal: make(chan struct{}, 3),
+		done: make(chan struct{}, 3)}
+	for i := 0; i < m.Count; i++ {
+		m.signal <- struct{}{}
+	}
+	for i := 0; i < 3; i++ {
+		go func() {
+			defer m.Unlock()
+			fmt.Println("Hello, 世界")
+			m.done <- struct{}{}
+		}()
+	}
+	m.Wait()
 }
